7b19dae35b3185ca72ca0c6b812653faa28b6da8
ChaeMoojin <Moojin@ChaeMoojins-MacBook-Pro.local>
2016-03-07 10:18:57 -0800

    apply airplane API

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -1,14 +1,14 @@
 class AirplanesController < ApplicationController
 	def index
 		t = (((Time.now.to_f * 1000.0).to_i % 100) - 50) * 0.001
-		arr = {:items => [{:id => "1", :latitude => 32.73 + t, :longitude => -117.19 - 2*t}, 
-											{:id => "2", :latitude => 32.74 - t, :longitude => -117.10 + 2*t},
-											{:id => "3", :latitude => 32.70 + 2*t, :longitude => -117.35 + t},
-											{:id => "4", :latitude => 32.76 - t, :longitude => -117.27 - t},
-											{:id => "5", :latitude => 32.68 + t, :longitude => -117.32 + t},
-											{:id => "6", :latitude => 32.73 - 2*t, :longitude => -117.15 + t},
-											{:id => "7", :latitude => 32.71 + t, :longitude => -117.33 - 2*t}
-										 ]}
+		arr = {:flightPositions => [{:flightId => "1", :positions => [{:lat => 32.73 + t, :lon => -117.19 - 2*t}]},
+																{:flightId => "2", :positions => [{:lat => 32.74 - t, :lon => -117.10 + 2*t}]},
+																{:flightId => "3", :positions => [{:lat => 32.70 + 2*t, :lon => -117.35 + t}]},
+																{:flightId => "4", :positions => [{:lat => 32.76 - t, :lon => -117.27 - t}]},
+																{:flightId => "5", :positions => [{:lat => 32.68 + t, :lon => -117.32 + t}]},
+																{:flightId => "6", :positions => [{:lat => 32.73 - 2*t, :lon => -117.15 + t}]},
+																{:flightId => "7", :positions => [{:lat => 32.71 + t, :lon => -117.33 - 2*t}]}
+																]}
 		respond_to do |format|
 			format.json { render :json => arr.to_json }
 		end
